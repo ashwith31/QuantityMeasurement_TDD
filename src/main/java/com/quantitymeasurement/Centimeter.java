56fd5b2a7f869653d4202d90fe8fe2cfd956cb44
ashwith31 <ashwitash@gmail.com>
2021-10-30 21:38:00 +0530

    Modified: Added code to make the test case pass for centimeter

--- src/main/java/com/quantitymeasurement/Centimeter.java
+++ src/main/java/com/quantitymeasurement/Centimeter.java
@@ -8,4 +8,12 @@ public class Centimeter {
     public Centimeter(double value) {
         this.value = value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Centimeter that = (Centimeter) o;
+        return Double.compare(that.value, value) == 0;
+    }
 }
